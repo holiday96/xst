0d0b7e42a46dd89f26e51faa9a382f5289c2cd88
ducbv-gmo <ducbv@runsystem.net>
2023-04-11 17:32:48 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout cho dự án

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import './App.css';
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+import { DefaultLayout } from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      Alo
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            // const Layout = route.layout === null ? Fragment : DefaultLayout;
+            const Page = route.component;
+
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
