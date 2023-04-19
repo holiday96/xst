5da990bed65e7c04315806ad2404f6fc7456c172
XuShiTa <namlun96@gmail.com>
2023-04-19 18:41:15 +0700

    80 - Áp Dụng PropTypes Vào Dự Án Tiktok

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import classNames from "classnames/bind";
 import Tippy from "@tippyjs/react/headless";
 import { useState } from "react";
@@ -51,7 +52,7 @@ function Menu({
           <PopperWrapper className={cx("menu-popper")}>
             {history.length > 1 && (
               <Header
-                title="Language"
+                title={current.title}
                 onBack={() => {
                   setHistory((prev) => prev.slice(0, prev.length - 1));
                 }}
@@ -67,5 +68,12 @@ function Menu({
     </Tippy>
   );
 }
+
+Menu.propTypes = {
+  children: PropTypes.node.isRequired,
+  items: PropTypes.array,
+  hideOnClick: PropTypes.bool,
+  onChange: PropTypes.func,
+};
 
 export default Menu;
